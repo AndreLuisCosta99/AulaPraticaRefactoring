61d466bd13cdcf9d7cedcf2f90980a5dc00b97e4
Dna157 <andrecosta.leao@gmail.com>
2022-12-05 22:45:01 -0300

    Commit 17

--- Movie.java
+++ Movie.java
@@ -28,30 +28,13 @@ public class Movie {
         }
        
   public double getCharge(int daysRented){
-    switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-              thisAmount += 2;
-           if (each.getDaysRented() > 2)
-              thisAmount += (each.getDaysRented() - 2) * 1.5;
-           break;
-        case Movie.NEW_RELEASE:
-           thisAmount += each.getDaysRented() * 3;
-           break;
-        case Movie.CHILDRENS:
-           thisAmount += 1.5;
-           if (each.getDaysRented() > 3)
-              thisAmount += (each.getDaysRented() - 3) * 1.5;
-            break;
-}
+    return _price.getCharge(daysRented);
+  
   }
 
 public int getFrequentRenterPoints(int daysRented){
-    int result = 0;
-    Enumeration rentals = _rentals.elements();
-    while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-        result += each.getFrequentRenterPoints();
-}
+    return _price.getFrequentRenterPoints(daysRented);
+ 
 }
 
 }
